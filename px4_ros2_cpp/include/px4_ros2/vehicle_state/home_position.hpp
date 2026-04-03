bbbdd1771a1fe6b2e888334c86808fd7b9333eba
dokyun-kim4 <donny5557@gmail.com>
2026-04-03 04:23:44 +0000

    fix for home position not publishing

--- px4_ros2_cpp/include/px4_ros2/vehicle_state/home_position.hpp
+++ px4_ros2_cpp/include/px4_ros2/vehicle_state/home_position.hpp
@@ -5,11 +5,11 @@
 
 #pragma once
 
-#include <Eigen/Eigen>
+#include <Eigen/Core>
 #include <px4_msgs/msg/home_position.hpp>
 #include <px4_ros2/common/context.hpp>
-#include <px4_ros2/utils/subscription.hpp>
 #include <px4_ros2/utils/message_version.hpp>
+#include <rclcpp/rclcpp.hpp>
 
 namespace px4_ros2
 {
@@ -22,85 +22,67 @@ namespace px4_ros2
  *
  * @ingroup vehicle_state
  */
-class HomePosition : public Subscription<px4_msgs::msg::HomePosition>
+class HomePosition
 {
 public:
   explicit HomePosition(Context & context)
-  : Subscription<px4_msgs::msg::HomePosition>(context,
-      "fmu/out/home_position" + +px4_ros2::getMessageNameVersion<px4_msgs::msg::HomePosition>()) {}
+  : _node(context.node())
+  {
+    std::string topic = context.topicNamespacePrefix() + "fmu/out/home_position" + px4_ros2::getMessageNameVersion<px4_msgs::msg::HomePosition>();
+    // Transient Local is required because home_position is published very infrequently (usually once at takeoff)
+    _sub = _node.create_subscription<px4_msgs::msg::HomePosition>(
+      topic,
+      rclcpp::QoS(1).best_effort().transient_local(),
+      [this](const px4_msgs::msg::HomePosition::SharedPtr msg) {
+        _last = *msg;
+        _received = true;
+      });
+  }
 
-  /**
-   * @brief Get the vehicle's home position in local coordinates.
-   *
-   * @return the local coordinates of the home position (NED) [m]
-   */
+  const px4_msgs::msg::HomePosition & last() const { return _last; }
+  
+  bool hasReceivedMessages() const { return _received; }
+
   Eigen::Vector3f localPosition() const
   {
-    const px4_msgs::msg::HomePosition & home = last();
-    return Eigen::Vector3f{home.x, home.y, home.z};
+    return Eigen::Vector3f{_last.x, _last.y, _last.z};
   }
 
-  /**
-   * @brief Get the vehicle's home position in global coordinates.
-   *
-   * @return the global coordinates of the home position (latitude [°], longitude [°], altitude [m AMSL])
-   */
   Eigen::Vector3d globalPosition() const
   {
-    const px4_msgs::msg::HomePosition & home = last();
-    return Eigen::Vector3d{home.lat, home.lon, home.alt};
+    return Eigen::Vector3d{_last.lat, _last.lon, _last.alt};
   }
 
-  /**
-   * @brief Get the vehicle's home position yaw.
-   *
-   * @return the yaw of the home position (NED) [rad]
-   */
   float yaw() const
   {
-    return last().yaw;
+    return _last.yaw;
   }
 
-  /**
-   * @brief Check if vehicle's local home position is valid (xyz).
-   *
-   * @return true if the local position has been set, otherwise false
-   */
   bool localPositionValid() const
   {
-    return lastValid() && last().valid_lpos;
+    return hasReceivedMessages() && _last.valid_lpos;
   }
 
-  /**
-   * @brief Check if vehicle's global horizontal home position is valid (lat, lon).
-   *
-   * @return true if latitude and longitude have been set, otherwise false
-   */
   bool globaHorizontalPositionValid() const
   {
-    return lastValid() && last().valid_hpos;
+    return hasReceivedMessages() && _last.valid_hpos;
   }
 
-  /**
-   * @brief Check if vehicle's home position altitude is valid.
-   *
-   * @return true if altitude has been set, otherwise false
-   */
   bool altitudeValid() const
   {
-    return lastValid() && last().valid_alt;
+    return hasReceivedMessages() && _last.valid_alt;
   }
 
-  /**
-   * @brief Check if home position has been set manually.
-   *
-   * @return true if set manually, otherwise false
-   */
   bool manualHome() const
   {
-    return last().manual_home;
+    return _last.manual_home;
   }
 
+private:
+  rclcpp::Node & _node;
+  typename rclcpp::Subscription<px4_msgs::msg::HomePosition>::SharedPtr _sub;
+  px4_msgs::msg::HomePosition _last;
+  bool _received{false};
 };
 
 /** @}*/
